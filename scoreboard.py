c366274075e31750bd76b82d1be45e5912e0b081
Omar Fourati <fourati.omar55@gmail.com>
2023-02-22 19:20:14 +0100

    saving highest score ever

--- scoreboard.py
+++ scoreboard.py
@@ -6,7 +6,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.highscore = 0
+        with open("data.txt") as data:
+            self.highscore = int(data.read())
         self.color("white")
         self.hideturtle()
         self.penup()
@@ -20,12 +21,12 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.highscore:
             self.highscore = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.highscore}")
         self.score = 0
         self.refresh_score()
 
     def increse_score(self):
         self.score += 1
         self.refresh_score()
-    # def game_over(self):
-    #     self.goto(0, 0)
-    #     self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+
